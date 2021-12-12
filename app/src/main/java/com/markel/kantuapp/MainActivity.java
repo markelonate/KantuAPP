298c1802694558d815b43c87beae1cf87f9bbb22
Markel <markel.onate@gmail.com>
2021-12-13 00:47:55 +0100

    Test

--- app/src/main/java/com/markel/kantuapp/MainActivity.java
+++ app/src/main/java/com/markel/kantuapp/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
     }
 
+
     @SuppressLint("NonConstantResourceId")
     BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
             item -> {
